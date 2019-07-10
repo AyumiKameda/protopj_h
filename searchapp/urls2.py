c514ab5b53cd72daefb95c56b34e92b951fd87bb
AyumiKameda <tiny_clown_tot@yahoo.co.jp>
2019-07-10 23:25:01 +0900

    My Django Girls app, first commit

---
+++ searchapp/urls2.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.conf.urls import url
+from . import views
+
+
+# どのアプリのurlsなのか分かるようにapp_nameを使用する。
+app_name = 'searchapp'
+
+urlpatterns = [
+    path('', views.SearchScreen.as_view(), name='search'),
+    path('result/<str:pk>/', views.ResultList.as_view(), name='result'),
+#    path('',views.details,name='details'),
+#    path('details/<str:pk>/',views.details_list,name='details'),
+    path('details/<str:pk>/', views.DetailsView.as_view(), name='details'),
+]
--- searchapp/views.py
+++ searchapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+#関数で書く場合
+"""
+def index(request):
+    return render(request, 'searchapp/index.html')
+"""
+
+#クラスで書く場合
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'searchapp/index.html')
+
+index = IndexView.as_view() #クラスベースのViewをView関数化するためのメソッド
